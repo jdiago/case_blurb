b522173f48a8544d5f115ef9be146def6a8566a9
David Small <mrdasmall@gmail.com>
2012-08-11 16:35:37 -0400

    Demonstrating using Nikogiri to manipulate an XML element.

--- esenem-xmpp-test.rb
+++ esenem-xmpp-test.rb
@@ -1,4 +1,6 @@
 require 'xmpp4r/client'
+require 'nokogiri'
+require 'pp'
 include Jabber # Makes using it a bit easier as we don't need to prepend Jabber:: to everything
 
 # Jabber::debug = true # Uncomment this if you want to see what's being sent and received!
@@ -9,7 +11,19 @@ cl.connect
 cl.auth(password)
 
 to = "JCe7zOAMux9Tk10@xmpp.esenem.com"
+flag = "something here for funzies"
 subject = "XMPP4R test"
-body = "Hi, this is my first try from XMPP4R!!!"
+body = "blahhhh"
 m = Message::new(to, body).set_type(:normal).set_id('1').set_subject(subject)
-cl.send m
+nkXML = (Nokogiri::XML.parse m.to_s).children.first
+
+pp nkXML
+#nkXML.[]=('randomAttr', 'random')
+(nkXML>'body')[0].content=('I\'m setting the content of this message, yo.')
+
+pp nkXML
+
+# Set variable for XML conversion to not add xmlns declaration at the top
+no_declaration = Nokogiri::XML::Node::SaveOptions::NO_DECLARATION
+newM = nkXML.to_xml(:save_with => no_declaration)
+cl.send newM
