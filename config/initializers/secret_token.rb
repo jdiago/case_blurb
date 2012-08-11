9430a1a234bf4221dda6975cc612cc77123a8728
Josef Diago <josef.diago@gmail.com>
2012-08-11 11:01:04 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaseBlurb::Application.config.secret_token = 'dc7b3091b874e1286ef045d9dc4ee8e9df1bd19b9977061659db9bd15175513d111bf56ebaf5e805242a63e63e06d6e1209094f3351ded2e3c563544339aecd3'
