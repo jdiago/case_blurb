48b7f3468ef467926959f09633ef1bcd5dc09886
Josef Diago <josef.diago@gmail.com>
2012-08-11 14:48:35 -0400

    user association with cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :username, :password, :password_confirmation
+
   has_secure_password
+
+  has_and_belongs_to_many :cases
 
   validates :username, presence: true, uniqueness: true
 end
