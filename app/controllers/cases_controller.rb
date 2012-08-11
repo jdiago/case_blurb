bf711d930d13dd6a93c2a29f0b6a353cca436ebe
Josef Diago <josef.diago@gmail.com>
2012-08-11 16:33:52 -0400

    cases

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,22 +1,38 @@
 class CasesController < ApplicationController
   def index
-  end
-
-  def create
+    @cases = Case.all
   end
 
   def new
+    @case = Case.new
   end
 
   def edit
+    @case = Case.find(params[:id])
   end
 
   def show
+    @case = Case.find(params[:id])
+  end
+
+  def create
+    @case = Case.new(params[:case])
+
+    if @case.save
+      redirect_to @case, notice: 'Case successfully created.'
+    else
+      render action:"new"
+    end
   end
 
   def update
+    @case = Case.find(params[:id])
   end
 
   def destroy
+    @case = Case.find(params[:id])
+    @case.destroy
+
+    redirect_to cases_url
   end
 end
--- app/models/case.rb
+++ app/models/case.rb
@@ -2,4 +2,6 @@ class Case < ActiveRecord::Base
   attr_accessible :name
 
   has_and_belongs_to_many :users
+
+  validates :name, presence: true, uniqueness: true
 end
