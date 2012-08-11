ef5f3aeffb31e1ebce7d59886745d5b832b6f098
Josef Diago <josef.diago@gmail.com>
2012-08-11 15:19:13 -0400

    blurb association

--- app/models/blurb.rb
+++ app/models/blurb.rb
@@ -1,3 +1,4 @@
 class Blurb < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :content
+  belongs_to :case
 end
--- db/migrate/20120811184552_create_blurbs.rb
+++ db/migrate/20120811184552_create_blurbs.rb
@@ -1,6 +1,8 @@
 class CreateBlurbs < ActiveRecord::Migration
   def change
     create_table :blurbs do |t|
+      t.text :content
+      t.references :case
 
       t.timestamps
     end
