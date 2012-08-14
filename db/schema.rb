c79f454baaa5be65784b88902b87d1259eb227e7
Josef Diago <josef.diago@gmail.com>
2012-08-14 17:34:45 -0400

    scaffold for docs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120811205443) do
+ActiveRecord::Schema.define(:version => 20120812154703) do
 
   create_table "blurbs", :force => true do |t|
     t.text     "content"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(:version => 20120811205443) do
   end
 
   create_table "cases", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "docs", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
