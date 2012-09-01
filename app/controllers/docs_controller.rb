05769445c26faea9662b8e24c05b0e7097750b36
Josef Diago <josef.diago@gmail.com>
2012-08-31 21:33:06 -0400

    case_docs crud

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,9 +1,9 @@
 class DocsController < ApplicationController
+  before_filter :find_case
   skip_before_filter :authorize
 
   def index
-    @case = Case.find(params[:case_id])
-    @case_docs = @case.docs
+    @docs = @case.docs
 
     respond_to do |format|
       format.html # index.html.erb
@@ -12,8 +12,7 @@ class DocsController < ApplicationController
   end
 
   def show
-    @case = Case.find(params[:case_id])
-    @case_doc = @case.docs.find(params[:id])
+    @doc = @case.docs.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -22,8 +21,7 @@ class DocsController < ApplicationController
   end
 
   def new
-    @case = Case.find(params[:case_id])
-    @case_doc = @case.docs.build
+    @doc = @case.docs.build
 
     respond_to do |format|
       format.html # new.html.erb
@@ -32,16 +30,15 @@ class DocsController < ApplicationController
   end
 
   def edit
-    @doc = Doc.find(params[:id])
+    @doc = @case.docs.find(params[:id])
   end
 
   def create
-    @case = Case.find(params[:case_id])
-    @case_doc = @case.docs.build(params[:case_doc])
+    @doc = @case.docs.build(params[:doc])
 
     respond_to do |format|
-      if @case_doc.save
-        format.html { redirect_to case_doc_path, notice: 'Doc was successfully created.' }
+      if @doc.save
+        format.html { redirect_to case_doc_path(@case, @doc), notice: 'Doc was successfully created.' }
         format.json { render json: @doc, status: :created, location: @doc }
       else
         format.html { render action: "new" }
@@ -51,11 +48,11 @@ class DocsController < ApplicationController
   end
 
   def update
-    @doc = Doc.find(params[:id])
+    @doc = @case.docs.find(params[:id])
 
     respond_to do |format|
       if @doc.update_attributes(params[:doc])
-        format.html { redirect_to @doc, notice: 'Doc was successfully updated.' }
+        format.html { redirect_to case_doc_path(@case, @doc), notice: 'Doc was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -69,8 +66,13 @@ class DocsController < ApplicationController
     @doc.destroy
 
     respond_to do |format|
-      format.html { redirect_to docs_url }
+      format.html { redirect_to case_docs_url }
       format.json { head :no_content }
     end
   end
+
+  protected
+    def find_case
+      @case = Case.find(params[:case_id])
+    end
 end
