6b8f35b8a7a7b9b20eae6e459d795c7a14da0914
Josef Diago <josef.diago@gmail.com>
2012-09-13 19:47:46 -0400

    model association dependent on delete

--- app/models/case.rb
+++ app/models/case.rb
@@ -2,8 +2,8 @@ class Case < ActiveRecord::Base
   attr_accessible :name
 
   has_and_belongs_to_many :users
-  has_many :docs
-  has_many :blurbs
+  has_many :docs, dependent: :destroy
+  has_many :blurbs, dependent: :destroy
 
   validates :name, presence: true
 end
